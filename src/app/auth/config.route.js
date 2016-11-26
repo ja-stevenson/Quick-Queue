77bd920da576c4dee0031c114aeaa341c81014d5
Jimmy Stevenson <jimmyastevenson@gmail.com>
2016-11-26 16:41:11 -0600

    login template html and route added

--- src/app/auth/config.route.js
+++ src/app/auth/config.route.js
@@ -13,6 +13,11 @@
       controller: 'AuthController',
       controllerAs: 'vm'
     });
+    $routeProvider.when('/login', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'AuthController',
+      controllerAs: 'vm'
+    })
   }
   
 })();
