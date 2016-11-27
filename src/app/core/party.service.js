a109cd6dd3dc054b357a62e268ac4fb6db334d89
Jimmy Stevenson <jimmyastevenson@gmail.com>
2016-11-27 14:05:13 -0600

    party service refactor continued and firebaseData factory started

--- src/app/core/party.service.js
+++ src/app/core/party.service.js
@@ -5,10 +5,13 @@
     .module('app.core')
     .factory('partyService', partyService);
   
-  function partyService() {
+  partyService.$inject = ['$firebaseArray', 'firebaseDataService'];
+  
+  function partyService($firebaseArray, firebaseDataService) {
     
     var service = {
-      Party: Party
+      Party: Party,
+      parties: $firebaseArray(firebaseDataService.root.child('parties'));
     };
     
     return service;
